d49f144d00bcb7825cec527e0d0348f4ad14563b
CapperKamiSama <casp6535@student.eadania.dk>
2018-09-19 13:04:34 +0200

    TutorialScene added. LoadSceneString script added

---
+++ SekuraCabins/Assets/Scripts/LoadSceneString.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneString : MonoBehaviour
+{
+
+    // Function to load a scene by a scene name
+    public void LoadByString(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
