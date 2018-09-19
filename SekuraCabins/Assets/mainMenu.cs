a054626403a6d12f29e4ddb7879dfeb4c3247d16
Edgard2 <edgardcamille@gmail.com>
2018-09-19 12:47:22 +0200

    Start menu created.

---
+++ SekuraCabins/Assets/mainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+    // load the tutorial scene
+	public void Tutorial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // load the training scene
+    public void Training()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void ScoreBord()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
