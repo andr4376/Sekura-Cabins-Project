206e8282761ea1f8cfa82f909e7be3d16fc33236
Unknown <andr4376@gmail.com>
2018-09-20 13:57:55 +0200

    "Working" Machine animation

---
+++ SekuraCabins/Assets/SpawnPrefab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPrefab : MonoBehaviour {
+
+
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private Transform position;
+
+    // Use this for initialization
+
+
+    public void SpawnObject()
+    {
+        GameObject go = GameObject.Instantiate(prefab, position);
+
+        go.transform.localScale = go.transform.localScale * 100;
+
+        Debug.Log("REEEEEEEEEEEEEEEE");
+    }
+}
