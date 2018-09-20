82ca1c8617549c794b188cefce0a05cc3c985f63
Unknown <andr4376@gmail.com>
2018-09-20 12:25:24 +0200

    ProtoType animation

---
+++ SekuraCabins/Assets/Prefabs/ProtoTypeAnimation/ScrewScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrewScript : MonoBehaviour {
+
+
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+    private void OnMouseDown()
+    {
+        animator.SetTrigger("Screw");
+    }
+}
