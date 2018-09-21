35b7401b3aa3ec11ca46a9544019294eee40e3eb
Unknown <andr4376@gmail.com>
2018-09-21 10:49:44 +0200

    Animation dissappears when done

---
+++ SekuraCabins/Assets/MachineScript.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class MachineScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private int steps = 4;
+    [SerializeField]
+    
+
+
+    public void DecrementStepCount()
+    {
+        steps--;
+
+        if (steps <= 0)
+        {
+            //Calls a method that runs paralel and destroys the gameobject 
+            //after an amount of seconds
+            StartCoroutine(DestroySelf());
+        }
+    }
+
+    private IEnumerator DestroySelf()
+    {
+        
+        yield return new WaitForSeconds(4);
+
+        Destroy(this.gameObject);
+    }
+
+
+}
